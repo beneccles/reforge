3b3c3b539f835b4dd3194018908c885a7c9f20c0
Ben Eccles <ben.g.eccles@icloud.com>
2019-10-01 11:51:47 -0600

    wrote the code needed to initialize the server,specified proxy and main inside of package.json.

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const massive = require('massive')
+const authCtrl = require('./controllers/authController')
+const {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env
+
+const app = express()
+
+app.use(express.json())
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET
+}))
+
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT} on station!`))
+})
